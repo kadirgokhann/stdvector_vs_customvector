6cba8a41a9dc64d07cbc1baa042b89636d13a7fd
Kadir Gökhan Sezer <74508086+kadirgokhann@users.noreply.github.com>
2024-12-15 15:21:40 +0300

    Create custom_vector_main.cpp

---
+++ custom_vector_main.cpp
@@ -0,0 +1,26 @@
+#include "Vector.h"
+
+int main() {
+
+    auto start = std::chrono::high_resolution_clock::now();
+    ////////////////////////////////////////////////////////////////////////////
+    Vector vec;
+    vec.resize(1000000000);
+
+    for (int i = 0; i < 1000000000; i++) {
+        vec.push_back(i * 1021 % 10) ;
+    }
+
+    int avg = 0;
+    for (int i = 0; i < 1000000000; i++) {
+        avg += vec[i];
+    }
+    avg /= 100000;
+    printf("Average: %d\n", avg);
+
+    ////////////////////////////////////////////////////////////////////////////
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Execution time: " << duration.count() << " microseconds, which is " << duration.count() / 1000000.0 << " seconds" << std::endl;
+    return 0;
+}
